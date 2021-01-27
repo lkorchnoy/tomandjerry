3968c3b87701b8cb7a9d5308b7c658258b515941
lkorchnoy <lkorchnoy@yahoo.com>
2021-01-26 23:10:30 -0500

    added circle in the HTML canvas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Game from './Game';
 class App extends Component {
--- src/components/game.js
+++ src/components/game.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 class Game extends Component {
-    ...
 state = {
-        mouse: {
+        jerry: {
             x: 50,
             y: this.refs.canvas.height/2,
             radius: 20
         }
     }
-...
+
+
+draw = () => {
+    const ctx = this.refs.canvas.getContext("2d");
+    ctx.fillStyle = "orange";
+        ctx.beginPath();
+        ctx.arc(this.state.bird.x, this.state.bird.y,                        
+                this.state.bird.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+    }
+    componentDidMount() {
+        this.draw()
+    }
+
+
     render() {
         return (
             <div> 
