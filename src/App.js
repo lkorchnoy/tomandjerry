39f808538aa3096f982b779aea946395998d2c05
lkorchnoy <lkorchnoy@yahoo.com>
2021-01-26 22:45:34 -0500

    added game component and changed app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
 
-export default class App extends React.Component {
-  render(){
-    return(
-      "Hello World"
-    )
+      </div>
+    );
   }
 }
+export default App;
 
